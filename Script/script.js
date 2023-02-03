7c9aa8027c859109977f27ebd3552289c2d31cc5
keginold philip <xkegix@users.noreply.github.com>
2023-02-03 11:06:53 +0000

    Code Done

--- Script/script.js
+++ Script/script.js
@@ -45,7 +45,37 @@ if (changeNeed.toFixed(2) === totalCashInDrawer.toFixed(2)) {
 
 // calculate change to give.
     // new array to track change.
+    // need to output - Currency type [i][0]. Value of Currency using Currency type. 
+    // Count for currency for number of currency types of specific type.
 let change = [];
-    
+    for (let i = cid.length - 1; i >= 0; i--) {
+        const currencyName = cid[i][0];
+        const currencyValue = currencyTypes[currencyName];
+        let currencyCount = 0;
+
+        // check changeNeed is more or = to currency value and if CID has money.
+            // true = reduce changeNeed by currencyValue.
+            // true = reduce CID by currencyValue.
+                // record of number of that currency being used as change.
+            while (changeNeed >= currencyValue && cid[i][1] > 0) {
+                changeNeed -= currencyValue;
+                changeNeed = changeNeed.toFixed(2);
+                cid[0][1] -= currencyValue;
+                currencyCount ++
+            }
+
+                // if more, push to change array. 
+                // display change as currency name, and amount.
+            if (currencyCount > 0){
+                change.push([currencyName, currencyValue * currencyCount])
+            }
+    }
+
+    //check if exact change can be given.
+    if (changeNeed > 0) {
+        return {status: "INSUFFICIENT_FUNDS", change: []};
+    }
+
+    return {status: "OPEN", change: change};
 
 }
